daf642ab0274a2c253ced774c09cd92bdb2f4bf6
Amit Varshney <amit.varshney@aftership.com>
2022-12-07 14:12:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { parse } from "json2csv";
+import FileDownload from "js-file-download";
+const cors = require('cors');
 
-function App() {
+function App () { 
+  const [user, setUser] = useState([]);
+
+ const fetchData = async (e) => {
+      const newres = await axios.get("https://api.aftership.com/v4/trackings/?tag=Exception", {
+        headers: {
+          "Content-Type": "application/json",
+          "aftership-api-key": "asat_4cdc59b79a484dfb83418a4975de728f"
+        },
+        responseType: "blob"
+      }).then((newres) => {
+        console.log(newres);
+        FileDownload(newres.data, "ExceptionReport.csv")})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Please click here to download the file</h1>
+    <button onClick={fetchData}> Download file </button>
+    </main>
   );
 }
 
